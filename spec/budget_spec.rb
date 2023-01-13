26d56a43688e7771ca953b8623898f81e8ef755f
Hady Matar <hadyematar@gmail.com>
2023-01-13 09:56:47 -0700

    Finish writing cheap deparments test

--- spec/budget_spec.rb
+++ spec/budget_spec.rb
@@ -6,6 +6,14 @@ describe Budget do
   context "Budget happy paths" do 
     let(:budget2020){Budget.new(2020)}
     let(:budget2021){Budget.new(2021)}
+    let(:customer_service){Department.new("Customer Service")}
+    let(:food){Department.new("Food")}
+    let(:recreation){Department.new("Recreation")}
+    let(:covid_relief){Department.new("Covid Relief")}
+    let(:health){Department.new("Health HS")}
+    let(:sanitation){Department.new("Sanitation")}
+    let(:law_enforcement){Department.new("Law Enforcement")}
+    let(:covid_recovery){Department.new("Covid Recovery")}
 
     it "exists as a budget object" do 
       expect(budget2020). to be_an_instance_of(Budget)
@@ -24,26 +32,51 @@ describe Budget do
 
     end
 
-    xit "can list the departments within its jurisdiction" do 
+    it "can list the departments within its jurisdiction" do 
+      
       budget2020.add_department(customer_service)
-      budget2020.add_department(sanitation)
-      budget2020.add_department(law_enforcement)
+      budget2020.add_department(food)
+      budget2020.add_department(recreation)
       budget2020.add_department(covid_relief)
 
-      budget2021.add_department(customer_service)
+      budget2021.add_department(health)
       budget2021.add_department(sanitation)
       budget2021.add_department(law_enforcement)
       budget2021.add_department(covid_recovery)
 
-      expect(budget2020.departments).to match_array([customer_service, sanitation, law_enforcement, covid_relief])
+      expect(budget2020.departments).to match_array([customer_service, food, recreation, covid_relief])
 
-      expect(budget2021.departments).to match_array([customer_service, sanitation, law_enforcement, covid_recovery])
+      expect(budget2021.departments).to match_array([health, sanitation, law_enforcement, covid_recovery])
 
     end
 
-    xit "can list all departments with expenses less than 500 dollars" do 
+    it "can list all departments with expenses less than 500 dollars" do 
 
-      expect(budget2020.cheap_departments).to match_array([customer_service, sanitation])
+      customer_service.expense(300)
+      customer_service.expense(100)
+      food.expense(700)
+      recreation.expense(50)
+      covid_relief.expense(400)
+      covid_relief.expense(300)
+
+      budget2020.add_department(customer_service)
+      budget2020.add_department(food)
+      budget2020.add_department(recreation)
+      budget2020.add_department(covid_relief)
+
+      health.expense(800)
+      sanitation.expense(600)
+      law_enforcement.expense(200)
+      law_enfocement.expense(100)
+      covid_recovery.expense(700)
+
+      budget2021.add_department(health)
+      budget2021.add_department(sanitation)
+      budget2021.add_department(law_enforcement)
+      budget2021.add_department(covid_recovery)
+
+
+      expect(budget2020.cheap_departments).to match_array([recreation, customer_service])
 
       expect(budget2021.cheap_departments).to match_array([law_enforcement])
 
