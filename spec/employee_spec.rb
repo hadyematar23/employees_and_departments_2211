86faae9dd7d8a7f5e770b67ad89337e1af003d5d
Hady Matar <hadyematar@gmail.com>
2023-01-13 09:04:43 -0700

    Set up some tests for iteration 1

---
+++ spec/employee_spec.rb
@@ -0,0 +1,21 @@
+require './lib/employee'
+
+describe Employee do
+  context "Employee happy paths" do 
+    let(:bobbi) { Employee.new({name: "Bobbi Jaeger", age: "30", salary: "$100000"})}
+
+    it "exists as an object of employee" do 
+
+      expect(bobbi).to be_an_instance_of(Employee)
+    end
+
+    it "has attributes including a name, age, salary" do 
+      expect(bobbi.name).to eq("Bobbi Jaeger")
+      expect(bobbi.age).to eq(30)
+      expect(bobbi.salary).to eq(100000)
+    end
+
+
+
+  end 
+end
